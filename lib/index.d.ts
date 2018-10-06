9dd7a65ffb73bcea56589007319e1a397218f697
exoticknight <draco.knight0@gmail.com>
2018-10-06 23:39:12 +0800

    refactor

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,161 +1,56 @@
-export declare type ResultKey = number | string;
-export declare type ResultItem = {
-    key: ResultKey;
-    data: any[];
+export declare type rawDatum = any;
+/**
+ * the string or number retrurned from Classifier
+ *
+ * 特征提取返回的值
+ */
+export declare type resultKey = number | string;
+/**
+ * result of classification
+ *
+ * 聚合的结果
+ *
+ * @member key characteristic of this group of rawdata
+ * @member rawdata array of rawdata that share the same characteristic
+ * @member value value of statistic
+ */
+export declare type resultItem = {
+    key: resultKey;
+    rawdata: rawDatum[];
     value: any;
 };
-export declare type Result = ResultItem[];
-export declare type ResultData = {
-    [key: number]: any[];
-    [key: string]: any[];
+export declare type dotPathStr = string;
+export declare enum Comparation {
+    '==' = 0,
+    '===' = 1,
+    '!=' = 2,
+    '!==' = 3,
+    '<' = 4,
+    '>' = 5,
+    '<=' = 6,
+    '>=' = 7
+}
+export declare type Assertion = dotPathStr | boolean | [dotPathStr, keyof typeof Comparation, string | number | boolean] | {
+    'and': Assertion[];
+} | {
+    'or': Assertion[];
+} | {
+    'not': Assertion;
 };
-/**
- * accessor that use dot operation to access data with path predefined
- *
- * 使用点操作符访问数据的访问器，访问路径已被定义
- *
- * @param {any} obj anythin that supports dot access
- * @returns {any} the value or undefined if value does't exist
- */
-export declare type DotAccessor = (obj: any) => any;
-/**
- * filter the datum and decide whether to accept it into statistic
- *
- * 过滤数据，决定是否接受此条目进行统计
- *
- * @param {any} datum the datum
- * @param {number} [index] index of the datum
- * @param {any[]} [array] the whole data array
- * @returns {boolean}
- */
-export declare type Filter = (datum?: any, index?: number, array?: any[]) => boolean;
-/**
- * transform raw data into desired output
- *
- * 将原始数据转换成期望的数据类型
- *
- * @param {any} rawdatum the raw datum
- * @param {number} [index] index of the datum
- * @param {any[]} [array] the whole data array
- * @returns {any}
- */
-export declare type Transformer = (rawdatum?: any, index?: number, array?: any[]) => any;
-/**
- * extract characteristic from datum
- *
- * 从数据里提取特征值
- *
- * @param {any} datum the datum
- * @param {number} [index] index of the datum
- * @param {any[]} [array] the whole data array
- * @returns {ResultKey}
- */
-export declare type Classifier = (datum: any, index?: number, array?: any[]) => ResultKey;
-/**
- * calculate current statistic result based on last result and current data
- *
- * 根据上一次统计结果和当前读入的数据计算当前的的统计结果
- *
- * @param {any} result last result of calculation
- * @param {any} current current datum
- * @param {number} count numbers of data that has been dealt with
- * @returns {any} result of current calculation
- */
-export declare type Calculator = (result: any, current?: any, count?: number) => any;
-/**
- * comparison function using in .sort() which is the same as Array.prototype.sort, except for requiring specified arguments.
- *
- * .sort() 中使用的比较函数，和 Array.prototype.sort 中一样，但是参数结构是特定的。
- *
- * @param {ResultItem} a operator a
- * @param {ResultItem} b operator b
- * @returns {boolean} result of comparison, -1 or 0 or 1
- */
-export declare type Comparator = (a: ResultItem, b: ResultItem) => 1 | 0 | -1;
-/**
- * the callback that runs before all sub-aggregation
- *
- * 在所有子聚合执行前调用的回调函数
- *
- * @param {ResultKey[]} args.indexes the index of aggregation's result
- * @param {Map<ResultKey, ResultItem>} args.items result of aggregation
- * @param {object} args.shared public variables that exist in the whole aggregation
- */
-export declare type BeforeCallbackFn = (args: {
-    indexes: ResultKey[];
-    items: Map<ResultKey, ResultItem>;
-    shared: object;
-}) => void;
-/**
- * the callback that runs after all sub-aggregation
- *
- * 在所有子聚合执行后调用的回调函数
- *
- * @param {ResultKey[]} args.indexes the index of aggregation's result
- * @param {Map<ResultKey, ResultItem>} args.items result of aggregation
- * @param {object} args.shared public variables that exist in the whole aggregation
- */
-export declare type AfterCallbackFn = (args: {
-    indexes: ResultKey[];
-    items: Map<ResultKey, ResultItem>;
-    shared: object;
-}) => void;
-/**
- * the callback that runs before each sub-aggregation
- *
- * 在每一个子聚合执行前调用的回调函数
- *
- * @param {ResultKey[]} args.index the index of item
- * @param {ResultItem} args.item contains key: the characteristic, data: result of classification, value: result of statistic
- * @param {object} args.shared public variables that exist in the whole aggregation
- */
-export declare type BeforeEachCallbackFn = (args: {
-    index: ResultKey;
-    item: ResultItem;
-    shared: object;
-}) => boolean | void;
-/**
- * the callback that runs after each sub-aggregation
- *
- * 在每一个子聚合执行后调用的回调函数
- *
- * @param {ResultKey[]} args.index the index of item
- * @param {ResultItem} args.item contains key: the characteristic, data: result of classification, value: result of statistic
- * @param {object} args.shared public variables that exist in the whole aggregation
- */
-export declare type AfterEachCallbackFn = (args: {
-    index: ResultKey;
-    item: ResultItem;
-    shared: object;
-}) => boolean | void;
-/**
- * generate an dot access function for specified path
- *
- * 生成一个使用点操作符访问对应路径数据的访问器
- *
- * @param {string} pathStr path string defined with dot access
- * @returns {DotAccessor} Fucntion
- */
-export declare function dotAccess(pathStr: string): DotAccessor;
 export declare class Aggregator {
-    _last: Aggregator;
-    context: {
-        _data: ResultData;
-        _shared: object;
-        _export: object;
-        _extract: Classifier;
-        _filter: Filter;
-        _agg?: Calculator;
-        _calc_base: any;
-        _sort?: Function;
-        _transformer?: Transformer;
-        _before?: Function;
-        _beforeEach?: Function;
-        _after?: Function;
-        _afterEach?: Function;
-    };
-    static from(data: any[]): Aggregator;
-    constructor(last?: Aggregator, data?: any[]);
+    private _last;
+    private context;
+    constructor(last?: Aggregator, initdata?: any[]);
+    /**
+     * pass a function to transform every datum before filtring
+     *
+     * 传入一个函数用以在过滤前对数据进行变换
+     *
+     * @param transformer function for transforming every datum
+     * @returns {this} this
+     * @memberof Aggregator
+     */
+    transform(transformer: (rawDatum?: rawDatum, index?: number, rawarray?: rawDatum[]) => any): this;
     /**
      * pass a function to filter data before aggregating
      *
@@ -165,40 +60,41 @@ export declare class Aggregator {
      * @returns {this} this
      * @memberof Aggregator
      */
-    filter(filter: Filter): this;
+    filter(filter: (rawDatum?: rawDatum, index?: number, rawarray?: rawDatum[]) => boolean): this;
     /**
-     * pass a function to transform every datum before aggregating
+     * filter builder using and / or / not assertion, conflicts with .filter
      *
-     * 传入一个函数用以在聚合前对数据进行变换
+     * 使用逻辑语句构建过滤器，不能和 .filter 一起使用
      *
-     * @param {Transformer} transformer function for transforming every datum
-     * @returns {this} this
+     * @example juhe.Assert.and([true, juhe.Assert.not(true)])
+     *
+     * @param {Assertion} assertion assertion
+     * @returns {this}
      * @memberof Aggregator
      */
-    transform(transformer: Transformer): this;
-    where(path: any, op: any, value: any): this;
+    where(assertion: Assertion): this;
     /**
      * use a function or dot access to extract characteristic for the classification later. You can access a field using dot access in 'path' to simplify your data in classifier
      *
-     * 使用一个函数，或者直接点访问，来将每一条数据进行特征提取。函数返回值将会作为分类的特征使用。可以在 path 中使用点操作符来简化 classifier 中数据访问
+     * 使用一个函数，或者直接指定路径访问，来将每一条数据进行特征提取。函数返回值将会作为分类的特征使用。可以在 path 中追加使用点操作符来简化 classifier 中数据访问
      *
-     * @param {(Classifier|string)} classifier function that take an input and output the characteristic, or just the path
+     * @param classifier function that take an input and output the characteristic, or just the path
      * @param {string} path optional, just provides a shortcut access for classifier
      * @returns {this} this
      * @memberof Aggregator
      */
-    classify(classifier: Classifier | string, path?: string): this;
+    classify(classifier: (datum: rawDatum, index?: number, array?: any[]) => resultKey | string, path?: string): this;
     /**
-     * use a function to do statistic for data in the same class
+     * use a function to do some calculation for data in the same class
      *
-     * 使用一个函数对同一类中的数据进行统计
+     * 使用一个函数对同一类中的数据进行计算
      *
-     * @param {Calculator} calculator a function to perform the statistical analysis
-     * @param {*} [base=0] initial result of statistic,default is 0
+     * @param calculator a function to perform the calculation
+     * @param {*} [base=0] initial result of calculation, default is 0
      * @returns {this} this
      * @memberof Aggregator
      */
-    calc(calculator: Calculator, base?: any): this;
+    calc(calculator: (lsatResult: any, current?: rawDatum, count?: number) => any, base?: any): this;
     /**
      * use a path to access value in datum and accumulate it. Shortcut of .calc((result, current) => result + current, 0)
      *
@@ -235,55 +131,71 @@ export declare class Aggregator {
      */
     avg(path?: string): this;
     /**
-     * use a comparison function to sort the index that will be provided to user
+     * use a comparison function to sort the index returned from the aggregation
      *
      * 使用一个比较函数对聚合结果进行排序
      *
-     * @param {Comparator} comparator the comparison function
+     * @param comparator the comparison function
      * @returns {this} this
      * @memberof Aggregator
      */
-    sort(comparator: Comparator): this;
+    sort(comparator: (a: resultItem, b: resultItem) => 1 | 0 | -1): this;
     /**
      * run a callback after current aggregation and before sub-aggregation. The callback will be called with {indexes, items, shared}
      *
-     * 在当前聚合执行后、子聚合执行前运行一个回调。回调的参数是 {indexes, items, shared}
+     * 在当前聚合执行后、子聚合执行前运行一个回调。回调的参数是 {indexes, resultMap, shared}
      *
-     * @param {BeforeCallbackFn} fn the callback
+     * @param callbackFn the callback that runs before all sub-aggregation
      * @returns {this} this
      * @memberof Aggregator
      */
-    before(fn: BeforeCallbackFn): this;
+    beforeAllSub(callbackFn: (args: {
+        indexes: resultKey[];
+        resultMap: Map<resultKey, resultItem>;
+        shared: object;
+    }) => void): this;
     /**
      * run a callback before each sub-aggregation. The callback will be called with {index, item, shared}
      *
-     * 在每一个子聚合前运行一个回调。回调的参数是 {index, item, shared}
+     * 在每一个子聚合前运行一个回调。回调的参数是 {index, result, shared}
      *
-     * @param {BeforeEachCallbackFn} fn the callback
+     * @param callbackFn the callback that runs before each sub-aggregation
      * @returns {this} this
      * @memberof Aggregator
      */
-    beforeEach(fn: BeforeEachCallbackFn): this;
+    beforeEachSub(callbackFn: (args: {
+        index: resultKey;
+        result: resultItem;
+        shared: object;
+    }) => boolean | void): this;
     /**
      * run a callback before each sub-aggregation. The callback will be called with {index, item, shared}
      *
-     * 在每一个子聚合后运行一个回调。回调的参数是 {index, item, shared}
+     * 在每一个子聚合后运行一个回调。回调的参数是 {index, result, shared}
      *
-     * @param {AfterEachCallbackFn} fn the callback
+     * @param callbackFn the callback that runs after each sub-aggregation
      * @returns {this} this
      * @memberof Aggregator
      */
-    afterEach(fn: AfterEachCallbackFn): this;
+    afterEachSub(callbackFn: (args: {
+        index: resultKey;
+        result: resultItem;
+        shared: object;
+    }) => boolean | void): this;
     /**
      * run a callback after sub-aggregation. The callback will be called with {indexes, items, shared}
      *
-     * 在子聚合执行后运行一个回调。回调的参数是 {indexes, items, shared}
+     * 在子聚合执行后运行一个回调。回调的参数是 {indexes, resultMap, shared}
      *
-     * @param {AfterCallbackFn} fn the callback
+     * @param callbackFn the callback that runs after all sub-aggregation
      * @returns {this} this
      * @memberof Aggregator
      */
-    after(fn: AfterCallbackFn): this;
+    afterAllSub(callbackFn: (args: {
+        indexes: resultKey[];
+        resultMap: Map<resultKey, resultItem>;
+        shared: object;
+    }) => void): this;
     /**
      *
      *
@@ -293,7 +205,7 @@ export declare class Aggregator {
      * @returns {this} this
      * @memberof Aggregator
      */
-    exportIndexAs(varname: string): this;
+    shareIndexAs(varname: string): this;
     /**
      * ⚠️: You shouldn't use this function
      *
@@ -301,7 +213,7 @@ export declare class Aggregator {
      *
      * @memberof Aggregator
      */
-    $execute(next: any): void;
+    private $execute;
     /**
      * execute the aggregation
      *
@@ -317,7 +229,7 @@ export declare class Aggregator {
      *
      * @memberof Aggregator
      */
-    destroy(): void;
+    private destroy;
     /**
      * 新建一个子聚合
      *
@@ -326,6 +238,18 @@ export declare class Aggregator {
      */
     sub(): Aggregator;
 }
-export declare const aggregator: {
-    from(data: any[]): Aggregator;
+export declare function from(data: rawDatum[]): Aggregator;
+export declare const Assert: {
+    and: (...args: Assertion[]) => Assertion;
+    or: (...args: Assertion[]) => Assertion;
+    not: (assertion: Assertion) => Assertion;
 };
+declare const _default: {
+    from: typeof from;
+    Assert: {
+        and: (...args: Assertion[]) => Assertion;
+        or: (...args: Assertion[]) => Assertion;
+        not: (assertion: Assertion) => Assertion;
+    };
+};
+export default _default;
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,5 +1,76 @@
-import {aggregator} from '../src/index'
+import test from 'tape'
+import juhe, {Assert} from '../src/index'
 
-const agg = aggregator.from([])
+const DATA = [
+  {
+    timestamp: new Date('2017-07-01'),
+    pv: 2246,
+    uv: 1345,
+    bool: false,
+  },
+  {
+    timestamp: new Date('2017-07-02'),
+    pv: 5730,
+    uv: 2550,
+    bool: true,
+  },
+  {
+    timestamp: new Date('2017-07-03'),
+    pv: 4567,
+    uv: 3656,
+    bool: false,
+  },
+  {
+    timestamp: new Date('2017-07-04'),
+    pv: 4679,
+    uv: 2366,
+    bool: true,
+  },
+  {
+    timestamp: new Date('2017-07-05'),
+    pv: 2904,
+    uv: 4832,
+    bool: false,
+  }
+]
 
-console.log(agg)
+test('transform', function(t) {
+  const agg = juhe.from(DATA)
+  agg
+  .transform((datum, i) => {
+    datum.index = i
+    return datum
+  })
+  .beforeAllSub(({indexes}) => {
+    t.plan(indexes.length)
+  })
+  .beforeEachSub(({index, result, shared}) => {
+    t.true(result.rawdata[0].hasOwnProperty('index'))
+  })
+  .execute()
+})
+
+test('where', function(t) {
+  const agg = juhe.from(DATA).where(
+    Assert.and(
+      true,
+      ['pv', '>=', 3000],
+      ['uv', '>=', 2000],
+      Assert.not(['timestamp', '>', +new Date('2017-07-03')])
+    )
+  )
+
+  const r0 = agg.context._filter(DATA[0])
+  const r1 = agg.context._filter(DATA[1])
+  const r2 = agg.context._filter(DATA[2])
+  const r3 = agg.context._filter(DATA[3])
+  const r4 = agg.context._filter(DATA[4])
+  t.false(r0, 'r0')
+  t.true(r1, 'r1')
+  t.true(r2, 'r2')
+  t.false(r3, 'r3')
+  t.false(r4, 'r4')
+
+  t.end()
+})
+
